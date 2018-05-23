693b9b42d7791c03c59a64feda990088de7d0b79
MagboneD <agbone.dm@gmail.com>
2018-05-23 06:43:53 -0700

    Add files via upload

---
+++ hello world.cs
@@ -0,0 +1,18 @@
+namespace HelloWorld
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text; 
+
+
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine;
+
+        }
+    }
+}
